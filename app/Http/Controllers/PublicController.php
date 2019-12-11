61f277c268829d6b26f70f9bc7bc38b6848dd6e3
“nevlund” <lund.privatmail@gmail.com>
2019-12-11 15:31:16 +0100

    updating controller file

--- app/Http/Controllers/PublicController.php
+++ app/Http/Controllers/PublicController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\http\Controllers\Controller;
+use App\Http\Controllers\Controller;
 use App\Post;
 
 class PublicController extends Controller
